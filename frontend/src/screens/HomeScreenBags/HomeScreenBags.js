3ce827dc5cac87039b384ffee1a2e94fce8c5bba
zubae <afroze@outlook.in>
2020-12-24 17:21:14 +0530

    spacing

--- frontend/src/screens/HomeScreenBags/HomeScreenBags.js
+++ frontend/src/screens/HomeScreenBags/HomeScreenBags.js
@@ -46,22 +46,22 @@ const HomeScreenBags = () => {
             </Row>
             <Row className='women-card-wrapper '>
                 {HomeScreenBagsProducts.map(product => (
-                   <div className='women-shop-bag-container-root' key={product.productId}>
-                    <Link to={`/product/${product._id}`}>
-                    <Col lg={2.6} className='women-shop-bag-container'>
-                    <div>
-                    <div className='women-card-image'>
-                        <div className='women-img-wrap'>
-                        <img id={product.productId} src={product.heroImage} alt='women_1'/>
-                        {product.bestSeller ? <span className='label-best'>{product.bestSeller}</span> : null}
-                        {product.quickView ? <span className='label-view' onMouseEnter={() => {handleMouseEnter(product)}} onMouseOut={() => {handleMouseOut(product)}}>{product.quickView}</span> : null}
-                        </div>
-                        <div className='women-card-title'>{product.productName}</div>
-                        <div className='women-card-text'>View Details - &#x20B9;{product.price ? product.price : product.mrpPrice}</div>
-                    </div>
-                    </div>
-                    </Col>
-                    </Link>
+                    <div className='women-shop-bag-container-root' key={product.productId}>
+                        <Link to={`/product/${product._id}`}>
+                            <Col lg={2.6} className='women-shop-bag-container'>
+                            <div>
+                                <div className='women-card-image'>
+                                    <div className='women-img-wrap'>
+                                        <img id={product.productId} src={product.heroImage} alt='women_1'/>
+                                        {product.bestSeller ? <span className='label-best'>{product.bestSeller}</span> : null}
+                                        {product.quickView ? <span className='label-view' onMouseEnter={() => {handleMouseEnter(product)}} onMouseOut={() => {handleMouseOut(product)}}>{product.quickView}</span> : null}
+                                    </div>
+                                    <div className='women-card-title'>{product.productName}</div>
+                                    <div className='women-card-text'>View Details - &#x20B9;{product.price ? product.price : product.mrpPrice}</div>
+                                </div>
+                            </div>
+                            </Col>
+                        </Link>
                     </div>
 
                     ))} 
