0bdc945d9ba9ffeabe4dcc335ce6c8885cfee122
zubae <afroze@outlook.in>
2020-12-24 13:04:07 +0530

    removed redux implementation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
-import store from './store'
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
 
