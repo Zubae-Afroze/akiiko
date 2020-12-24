3c56b283cc1f15d86739dc4f16f77aa7ab50660d
zubae <afroze@outlook.in>
2020-12-24 13:01:54 +0530

    constant rename

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODCUT_LIST_FAIL} from '../constants/productReducerConstant'
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productReducerConstant'
 
 export const productReducer = (state = { products: []}, action) => {
     switch (action.type) {
@@ -6,7 +6,7 @@ export const productReducer = (state = { products: []}, action) => {
             return  { loading: true, products: [] }
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload }
-        case PRODCUT_LIST_FAIL:
+        case PRODUCT_LIST_FAIL:
             return { loading: false, error: action.payload }
         default:
                 return state
