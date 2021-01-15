0d91919e6228725883df74e5f7176b20edf5e357
zubae <afroze@outlook.in>
2021-01-15 12:41:42 +0530

    Loading Done

--- frontend/src/screens/HomeScreenBags/HomeScreenBags.js
+++ frontend/src/screens/HomeScreenBags/HomeScreenBags.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import MyComponent from 'react-fullpage-custom-loader';
+import SpinnerIcon from '../../components/Spinner/SpinnerIcon';
+
 //Action
 import { actionListHomeBags } from '../../actions/actionHomeBags'
 
@@ -14,11 +17,11 @@ import './HomeScreenBags.css';
 
 const HomeScreenBags = () => {
     const handleMouseEnter = (product) => {
-        document.getElementById(product.productId).src=product.hoverImage
+        document.getElementById(product.productId).src = product.hoverImage
     }
 
     const handleMouseOut = (product) => {
-        document.getElementById(product.productId).src=product.images[0];
+        document.getElementById(product.productId).src = product.images[0];
     }
 
     //const [HomeScreenBagsProducts, setHomeScreenBagsProducts] = useState([]);
@@ -27,57 +30,63 @@ const HomeScreenBags = () => {
 
     const homeScreenBags = useSelector(state => state.homeScreenBags);
 
-    const { loading, error, products } = homeScreenBags 
+    const { loading, error, products } = homeScreenBags
 
-    useEffect(() => {   
+    useEffect(() => {
         dispatch(actionListHomeBags())
     }, [dispatch])
 
     //const HomeScreenBagsProducts = []
 
     return (
-        <div> 
-                <Row className='women-wrapper'>
+        <div>
+            <Row className='women-wrapper'>
                 <Col sm={9} className='women-wrap'>
                     <div className='home-title women-title'>BAGS </div>
-                    <div className='women-text'>A delightful range of sustainable bags that are designed 
-                        to perfection. Minimal, yet classic silhouettes to 
-                        compliment just about every look, 
+                    <div className='women-text'>A delightful range of sustainable bags that are designed
+                    to perfection. Minimal, yet classic silhouettes to
+                    compliment just about every look,
                         multipurpose & eye-catching.</div>
-                    
+
                 </Col>
                 <Col sm={3} className='women-button-placement'>
                     <Link to='/allproducts/productlist/bags'><button className='women-button'>SHOP ALL</button></Link>
                 </Col>
             </Row>
-            { loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
-            <Row className='women-card-wrapper '>
-                {products.map(product => (
-                    <div className='women-shop-bag-container-root' key={product.productId}>
-                        <Link to={`/product/${product._id}`}>
-                            <Col lg={2.6} className='women-shop-bag-container'>
-                            <div>
-                                <div className='women-card-image'>
-                                    <div className='women-img-wrap'>
-                                        <img id={product.productId} src={product.heroImage} alt='women_1'/>
-                                        {product.bestSeller ? <span className='label-best'>{product.bestSeller}</span> : null}
-                                        {product.quickView ? <span className='label-view' onMouseEnter={() => {handleMouseEnter(product)}} onMouseOut={() => {handleMouseOut(product)}}>{product.quickView}</span> : null}
-                                    </div>
-                                    <div className='women-card-title'>{product.productName}</div>
-                                    <div className='women-card-text'>View Details - &#x20B9;{product.price ? product.price : product.mrpPrice}</div>
-                                </div>
+            { loading ? <MyComponent
+                sentences={[]}
+                wrapperBackgroundColor={'rgba(255,255,255)'}
+                color={'#6e4e37'}
+                loaderType={'ball-spin-clockwise'}
+                customLoader={<SpinnerIcon />}
+            /> : error ? <h3>{error}</h3> :
+                    <Row className='women-card-wrapper '>
+                        {products.map(product => (
+                            <div className='women-shop-bag-container-root' key={product.productId}>
+                                <Link to={`/product/${product._id}`}>
+                                    <Col lg={2.6} className='women-shop-bag-container'>
+                                        <div>
+                                            <div className='women-card-image'>
+                                                <div className='women-img-wrap'>
+                                                    <img id={product.productId} src={product.heroImage} alt='women_1' />
+                                                    {product.bestSeller ? <span className='label-best'>{product.bestSeller}</span> : null}
+                                                    {product.quickView ? <span className='label-view' onMouseEnter={() => { handleMouseEnter(product) }} onMouseOut={() => { handleMouseOut(product) }}>{product.quickView}</span> : null}
+                                                </div>
+                                                <div className='women-card-title'>{product.productName}</div>
+                                                <div className='women-card-text'>View Details - &#x20B9;{product.price ? product.price : product.mrpPrice}</div>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Link>
                             </div>
-                            </Col>
-                        </Link>
-                    </div>
 
-                    ))} 
-            </Row> }
-                <div className='hero-button mobile-view-button'>
-                        <Link to='/allproducts/productlist/bags'><button>
-                            Shop Now
+                        ))}
+                    </Row>}
+            <div className='hero-button mobile-view-button'>
+                <Link to='/allproducts/productlist/bags'><button>
+                    Shop Now
                         </button></Link>
-                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/screens/HomeScreenStorage/HomeScreenStorage.js
+++ frontend/src/screens/HomeScreenStorage/HomeScreenStorage.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {  Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import { actionListHomeStorage } from '../../actions/actionHomeStorage';
+
+import MyComponent from 'react-fullpage-custom-loader';
+import SpinnerIcon from '../../components/Spinner/SpinnerIcon';
 
 //import HomeScreenStorageProducts from '../../assets/products/HomeScreenStorageProducts';
 import './HomeScreenStorage.css';
@@ -13,11 +16,11 @@ import './HomeScreenStorage.css';
 const HomeScreenWorkout = () => {
 
     const handleMouseEnter = (product) => {
-        document.getElementById(product.productId).src=product.hoverImage
+        document.getElementById(product.productId).src = product.hoverImage
     }
 
     const handleMouseOut = (product) => {
-        document.getElementById(product.productId).src=product.images[0];
+        document.getElementById(product.productId).src = product.images[0];
     }
 
     //const [HomeScreenStorageProducts, setHomeScreenStorageProducts] = useState([]);
@@ -37,40 +40,46 @@ const HomeScreenWorkout = () => {
     return (
         <div className='home-screen-component'>
             <Row className='home-wrapper workout-cont'>
-                <Col sm={3} className='home-button-placement'> 
-                    <Link to='/allproducts/productlist/storage'><button className='home-button home-workout-button'>SHOP ALL</button></Link> 
+                <Col sm={3} className='home-button-placement'>
+                    <Link to='/allproducts/productlist/storage'><button className='home-button home-workout-button'>SHOP ALL</button></Link>
                 </Col>
                 <Col sm={9} className='home-wrapper'>
                     <div className='home-title'>STORAGE</div>
-                    <div className='home-text'>A thoughtful range of organisers and storage bags to compliment and increase the functionality of your home. 
+                    <div className='home-text'>A thoughtful range of organisers and storage bags to compliment and increase the functionality of your home.
                     Simplistic and warm, the colour tones and minimal design elevate the mood of your personal spaces and allow for easy declaring. </div>
                 </Col>
             </Row>
             <Row>
-                <Col sm={5}  className='hero-image-workout'> 
+                <Col sm={5} className='hero-image-workout'>
 
                     <div className='workout-hero-image'>
-                        <img src={'/images/home_screen_images/HomeScreenStorageHero.jpg'} alt='home_hero_img'/>
+                        <img src={'/images/home_screen_images/HomeScreenStorageHero.jpg'} alt='home_hero_img' />
                     </div>
                 </Col>
-                { loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
-                <Col sm={7} className='workout-container'>
-                    <Row className='workout-card-wrapper'>
-                        {products.map(product => (
-                            <Col className='home-card-items ' key={product._id}>
-                                <Link to={`/product/${product._id}`}><div>
-                                    <div className='women-img-wrap'>
-                                    <img id={product.productId} className='home-card-image' src={product.heroImage} alt='home_1'/>
-                                    {product.bestSeller ? <span className='label-best label-best-workout'>{product.bestSeller}</span> : null}
-                                    {product.quickView ? <span className='label-view label-view-workout' onMouseEnter={() => {handleMouseEnter(product)}} onMouseOut={() => {handleMouseOut(product)}}>{product.quickView}</span> : null}
-                                    </div>
-                                    <div className='home-card-title'>{product.productName}</div>
-                                <div className='home-card-text'>View Details - &#x20B9;{product.price ? product.price : product.mrpPrice}</div>
-                                </div></Link>
-                            </Col>
-                        ))}
-                    </Row>
-                </Col> }
+                {loading ? <MyComponent
+                    sentences={[]}
+                    wrapperBackgroundColor={'rgba(255,255,255)'}
+                    color={'#6e4e37'}
+                    loaderType={'ball-spin-clockwise'}
+                    customLoader={<SpinnerIcon />}
+                /> : error ? <h3>{error}</h3> :
+                        <Col sm={7} className='workout-container'>
+                            <Row className='workout-card-wrapper'>
+                                {products.map(product => (
+                                    <Col className='home-card-items ' key={product._id}>
+                                        <Link to={`/product/${product._id}`}><div>
+                                            <div className='women-img-wrap'>
+                                                <img id={product.productId} className='home-card-image' src={product.heroImage} alt='home_1' />
+                                                {product.bestSeller ? <span className='label-best label-best-workout'>{product.bestSeller}</span> : null}
+                                                {product.quickView ? <span className='label-view label-view-workout' onMouseEnter={() => { handleMouseEnter(product) }} onMouseOut={() => { handleMouseOut(product) }}>{product.quickView}</span> : null}
+                                            </div>
+                                            <div className='home-card-title'>{product.productName}</div>
+                                            <div className='home-card-text'>View Details - &#x20B9;{product.price ? product.price : product.mrpPrice}</div>
+                                        </div></Link>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Col>}
             </Row>
             <div className='hero-button mobile-view-button'>
                 <Link to='/storage/all_products'><button>
