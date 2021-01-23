db5cc967299c6ef21de590f1378a5ce8ffc9c7b0
zubae <afroze@outlook.in>
2021-01-23 12:53:11 +0530

    prod init

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 
@@ -43,6 +44,20 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
 // app.get('/api/config/razorpay', (req, res) => res.send(process.env.RAZOR_PAY_KEY))
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
+
+
 
 app.use(notFound)
 app.use(errorHandler)
