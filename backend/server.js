7588db2e011b0fe648f03a57da8bb1f3ae2c30f6
zubae <zubae@live.in>
2021-07-07 18:03:11 +0530

    comment

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,46 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
 
-import HomeScreenBagsProducts from './data/HomeScreenBagsProducts.js';
-import HomeScreenHomeProducts from './data/HomeScreenHomeProducts.js';
-import HomeScreenStorageProducts from './data/HomeScreenStorageProducts.js';
+import HomeScreenBagsProducts from './data/HomeScreenBagsProducts.js'
+import HomeScreenHomeProducts from './data/HomeScreenHomeProducts.js'
+import HomeScreenStorageProducts from './data/HomeScreenStorageProducts.js'
 
-import connectDb from './config/db.js';
+import connectDb from './config/db.js'
 
-import masterProductRoutes from './routes/masterProductRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
+import masterProductRoutes from './routes/masterProductRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import router from './routes/masterProductRoutes.js';
+import router from './routes/masterProductRoutes.js'
 
 // git commands
 // git push heroku master
-// git push origin master
+// git push origin
+// git remote add origin https://github.com/Zubae-Afroze/akiiko-ver-1.05-fullstack.git
 
-dotenv.config();
+dotenv.config()
 
-connectDb();
+connectDb()
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 //Getting All the Data from HomeScreenBagProducts
 app.get('/api/home_screen_bags_products', (req, res) => {
-    res.json(HomeScreenBagsProducts);
+  res.json(HomeScreenBagsProducts)
 })
 
 //Getting All the Data from HomeScreenHomeProducts
 app.get('/api/home_screen_home_products', (req, res) => {
-    res.json(HomeScreenHomeProducts);
+  res.json(HomeScreenHomeProducts)
 })
 
 //Getting All the Data from HomeScreenStorageProducts
 app.get('/api/home_screen_storage_products', (req, res) => {
-    res.json(HomeScreenStorageProducts);
+  res.json(HomeScreenStorageProducts)
 })
 
 app.use('/api/product', masterProductRoutes)
@@ -52,20 +53,23 @@ app.use('/api/orders', orderRoutes)
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running...')
-    })
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
 }
-
-
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
