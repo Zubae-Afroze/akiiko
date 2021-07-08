e85568919a9251d24dcb5b26b1ffcc5076fa265f
zubae <zubae@live.in>
2021-07-08 16:26:36 +0530

    usermodel changed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,51 +1,56 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 import bycrypt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phoneNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false
+      type: Boolean,
+      required: true,
+      default: false,
     },
-    shippingAddress: {
+    shippingAddress: [
+      {
         address: { type: String },
         city: { type: String },
-        postalCode: { type: String }
-    },
-}, {
-    timestamps: true
-})
+        postalCode: { type: String },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bycrypt.compare(enteredPassword, this.password)
+  return await bycrypt.compare(enteredPassword, this.password)
 }
 
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next()
-    }
+  if (!this.isModified('password')) {
+    next()
+  }
 
-    const salt = await bycrypt.genSalt(10)
-    this.password = await bycrypt.hash(this.password, salt)
+  const salt = await bycrypt.genSalt(10)
+  this.password = await bycrypt.hash(this.password, salt)
 })
 
-const UserModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model('User', userSchema)
 
 export default UserModel
