60363a66ead60af865e6a202911cc06adc623b60
zubae <zubae@live.in>
2021-07-12 18:14:42 +0530

    removed firebase redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,45 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Provider, useSelector } from 'react-redux'
-import { ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase'
+import { Provider } from 'react-redux'
 import store from './store'
 
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-import firebase from 'firebase/app'
-
-import MyComponent from 'react-fullpage-custom-loader'
-import SpinnerIcon from './components/Spinner/SpinnerIcon'
-
-const rrfProps = {
-  firebase,
-}
-
-function AuthLoaded({ children }) {
-  const auth = useSelector((state) => state.firebase.auth)
-  if (isLoaded(auth))
-    return (
-      <MyComponent
-        sentences={[]}
-        wrapperBackgroundColor={'rgba(255,255,255)'}
-        color={'#6e4e37'}
-        loaderType={'ball-spin-clockwise'}
-        customLoader={<SpinnerIcon />}
-      />
-    )
-  return children
-}
-
 ReactDOM.render(
   <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <AuthLoaded>
-        <App />
-      </AuthLoaded>
-    </ReactReduxFirebaseProvider>
+    <App />
   </Provider>,
   document.getElementById('root')
 )
