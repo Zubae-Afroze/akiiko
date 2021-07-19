cdfe848bee3e19401d505ccd049835ed2e3915c6
surya <samsurya336@gmail.com>
2021-07-19 10:11:23 +0530

    Test Checkout

--- frontend/src/screens/Test Checkout/shippingForm.js
+++ frontend/src/screens/Test Checkout/shippingForm.js
@@ -7,125 +7,263 @@ export default function ShippingFormT() {
             <Row className='p-0 m-0'>
 
                 <NameField />
+                
+                <DummyHeight />
 
                 <AddressField />
 
                 <PhoneNumberField />
+                
+                <DummyHeight />
+
+                <CityComp />
+
+                <StateField />
+
+                <DummyHeightVisibleOnlyOnXtraSmallScreen />
+
+                <PinCodeField />
 
             </Row>
         </>
     )
+
+    function DummyHeight() {
+        return(
+            <Col xs={12}>
+                <div style={{height: '5px'}}/>
+            </Col>
+        )
+    }
+
+
+    function DummyHeightVisibleOnlyOnXtraSmallScreen() {
+        return(
+            <Col xs={12} className='d-block d-sm-none '>
+                <div style={{height: '5px'}}/>
+            </Col>
+        )
+    }
+
+    function NameField() {
+        console.log('Name Field Re-Build')
+        return(
+            <>
+                <Col xs={6} lg={6} className='p-0 m-0'>
+                        
+                        <Form.Control
+                            size='sm'
+                            type='text'
+                            placeholder='First Name'
+                            name='firstname'
+                            // value={shippingObject.firstname}
+                            // onChange={handelInputeChange}
+                            className='d-block d-sm-none '
+                        />
+    
+                        <Form.Control
+                            size='lg'
+                            type='text'
+                            placeholder='First Name'
+                            name='firstname'
+                            // value={shippingObject.firstname}
+                            // onChange={handelInputeChange}
+                            className='d-none d-sm-block '
+                        />
+    
+                    </Col>
+    
+                    <Col xs={6} lg={6} className='p-0 m-0'>
+                        
+                        <Form.Control
+                            size='sm'
+                            type='text'
+                            placeholder='Last Name'
+                            name='firstname'
+                            // value={shippingObject.firstname}
+                            // onChange={handelInputeChange}
+                            className='d-block d-sm-none '
+                        />
+    
+                        <Form.Control
+                            size='lg'
+                            type='text'
+                            placeholder='Last Name'
+                            name='firstname'
+                            // value={shippingObject.firstname}
+                            // onChange={handelInputeChange}
+                            className='d-none d-sm-block '
+                        />
+    
+                    </Col>
+            </>
+        )
+    }
+
+
+
+
+    function AddressField() {
+        return(
+            <Col xs={6} lg={6} className='p-0 m-0'>
+                        
+                <Form.Control
+                    size='sm'
+                    type='text'
+                    placeholder='Address'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-block d-sm-none '
+                />
+    
+                <Form.Control
+                    size='lg'
+                    type='text'
+                    placeholder='Address'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-none d-sm-block '
+                />
+    
+            </Col>
+        )
+    }
+
+
+
+
+    function PhoneNumberField() {
+        return(
+            <Col xs={6} className='p-0 m-0'>
+                        
+                <Form.Control
+                    size='sm'
+                    type='text'
+                    placeholder='Phone Number'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-block d-sm-none '
+                />
+    
+                <Form.Control
+                    size='lg'
+                    type='text'
+                    placeholder='Phone Number'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-none d-sm-block '
+                />
+    
+            </Col>
+        )
+    }
+
+
+
+    function CityComp() {
+        return(
+            <Col xs={6} sm={4} className='p-0 m-0'>
+                        
+                <Form.Control
+                    size='sm'
+                    type='text'
+                    placeholder='City'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-block d-sm-none '
+                />
+    
+                <Form.Control
+                    size='lg'
+                    type='text'
+                    placeholder='City'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-none d-sm-block '
+                />
+    
+            </Col>
+        )
+    }
+    
+    
+    
+    function StateField() {
+        return(
+            <Col xs={6} sm={4} className='p-0 m-0'>
+                        
+                <Form.Control
+                    size='sm'
+                    type='text'
+                    placeholder='State'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-block d-sm-none '
+                />
+    
+                <Form.Control
+                    size='lg'
+                    type='text'
+                    placeholder='State'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-none d-sm-block '
+                />
+    
+            </Col>
+        )
+    }
+    
+    
+
+    
+    function PinCodeField() {
+        return(
+            <Col xs={12} sm={4} className='p-0 m-0'>
+                        
+                <Form.Control
+                    size='sm'
+                    type='text'
+                    placeholder='Pin Code'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-block d-sm-none '
+                />
+    
+                <Form.Control
+                    size='lg'
+                    type='text'
+                    placeholder='Pin Code'
+                    name='firstname'
+                    // value={shippingObject.firstname}
+                    // onChange={handelInputeChange}
+                    className='d-none d-sm-block '
+                />
+    
+            </Col>
+        )
+    }
+
 }
 
 
-function NameField() {
-    return(
-        <>
-            <Col xs={12} lg={6} className='p-0 m-0'>
-                    
-                    <Form.Control
-                        size='md'
-                        type='text'
-                        placeholder='First Name'
-                        name='firstname'
-                        // value={shippingObject.firstname}
-                        // onChange={handelInputeChange}
-                        className='d-block d-sm-none '
-                    />
-
-                    <Form.Control
-                        size='lg'
-                        type='text'
-                        placeholder='First Name'
-                        name='firstname'
-                        // value={shippingObject.firstname}
-                        // onChange={handelInputeChange}
-                        className='d-none d-sm-block '
-                    />
-
-                </Col>
-
-                <Col xs={12} lg={6} className='p-0 m-0'>
-                    
-                    <Form.Control
-                        size='md'
-                        type='text'
-                        placeholder='Last Name'
-                        name='firstname'
-                        // value={shippingObject.firstname}
-                        // onChange={handelInputeChange}
-                        className='d-block d-sm-none '
-                    />
-
-                    <Form.Control
-                        size='lg'
-                        type='text'
-                        placeholder='Last Name'
-                        name='firstname'
-                        // value={shippingObject.firstname}
-                        // onChange={handelInputeChange}
-                        className='d-none d-sm-block '
-                    />
-
-                </Col>
-        </>
-    )
-}
-
-function AddressField() {
-    return(
-        <Col xs={12} lg={6} className='p-0 m-0'>
-                    
-            <Form.Control
-                size='md'
-                type='text'
-                placeholder='Address'
-                name='firstname'
-                // value={shippingObject.firstname}
-                // onChange={handelInputeChange}
-                className='d-block d-sm-none '
-            />
-
-            <Form.Control
-                size='lg'
-                type='text'
-                placeholder='Address'
-                name='firstname'
-                // value={shippingObject.firstname}
-                // onChange={handelInputeChange}
-                className='d-none d-sm-block '
-            />
-
-        </Col>
-    )
-}
 
 
 
-function PhoneNumberField() {
-    return(
-        <Col xs={12} lg={6} className='p-0 m-0'>
-                    
-            <Form.Control
-                size='md'
-                type='text'
-                placeholder='Phone Number'
-                name='firstname'
-                // value={shippingObject.firstname}
-                // onChange={handelInputeChange}
-                className='d-block d-sm-none '
-            />
 
-            <Form.Control
-                size='lg'
-                type='text'
-                placeholder='Phone Number'
-                name='firstname'
-                // value={shippingObject.firstname}
-                // onChange={handelInputeChange}
-                className='d-none d-sm-block '
-            />
 
-        </Col>
-    )
-}
+
+
+
+
+
+
