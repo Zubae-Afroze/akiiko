e29986b05a08ab531e9e5e596d6a899426ae3331
surya <samsurya336@gmail.com>
2021-08-24 14:53:41 +0530

    Done

--- frontend/src/components/Checkout Stable/OrderSummaryComp.js
+++ frontend/src/components/Checkout Stable/OrderSummaryComp.js
@@ -9,13 +9,23 @@ import '../../screens/Stable Checkout Screen/CheckOutStlye.css'
 import { Link } from 'react-router-dom'
 
 export default function OrderSummaryCompT() {
+  const cartList = useSelector((state) => state.cartList)
 
-    const cartList = useSelector(state => state.cartList)
+  const checkOutFormObj = useContext(CheckOutFormContext)
 
-    const history = useHistory()
+  return (
+    <>
+      <div style={{ width: '100%' }}>
+        <h6>Order Summary</h6>
+        <hr />
+        <CartItemsComp />
+        <hr />
+        <PriceDetailsComp />
+      </div>
+    </>
+  )
 
-    const checkOutFormObj = useContext(CheckOutFormContext);
-
+  function CartItemsComp() {
     return (
       <div>
         <Row className='m-0 p-0'>
@@ -77,11 +87,11 @@ export default function OrderSummaryCompT() {
           </h6>
         </div>
         <div style={{ height: '65px' }} />
-        <div className='d-flex justify-content-center continue-shopping-style'>
+        <div className='d-flex justify-content-center'>
           <Link to='/search'>
-            {/* <button type='button'> */} {/*btn btn-outline-secondary*/}
+          <button type='button' className='orderSummary-continue-shooping-btn'> {/*btn btn-outline-secondary*/}
             Continue Shopping
-            {/* </button> */}
+            </button>
           </Link>
         </div>
         <div style={{ height: '65px' }} />
@@ -89,89 +99,11 @@ export default function OrderSummaryCompT() {
     )
   }
 
-    function CartItemsComp(){
-    
-        return(
-            <div>
-                <Row className='m-0 p-0'>
-                {cartList.cartItems.map((item, index) => {
-                    return (
-                        <ItemCardT item={item}/>
-                    );
-                })}
-                </Row>
-            </div>
-        );
-    }
-
-
-    function PriceDetailsComp(){
-        console.log('COD: '+checkOutFormObj.formObject.isCodChecked )
-        return(
-            <div>
-                <div className='f-f d-flex justify-content-between'>
-                <h6>Subtotal</h6>
-                <h6>{cartList.cartItems.reduce((acc, items) => acc + items.qty * items.price, 0)}</h6>
-                </div>
-                <div className='f-f d-flex justify-content-between'>
-                <h6>Additional Pay</h6>
-
-                {
-                    /* cartList.cartItems.reduce((acc, items) => acc + items.qty * items.price, 0) > 500  ? */
-                    isCodCheckedFunction() ?
-                    <h6>50</h6>
-                    : <h6>--</h6>
-                }
-
-                </div>
-                <div className='f-f d-flex justify-content-between'>
-                <h6>Shipping</h6>
-                
-                {   
-                    /* checkOutFormObj.formObject.isCodChecked  */
-                    checkPriceIsBelow500()
-                    ? <h6>50</h6>
-                    : <h6>--</h6>
-                }
-                
-                
-                </div>
-                <hr />
-
-                <div className='d-flex justify-content-between'>
-                <h6 className='f-f-m'>Total</h6>
-
-                <h6 className='f-f-m'>
-                    {
-                        cartList.cartItems.reduce((acc, items) => acc + items.qty * items.price, 0) +
-                        (isCodCheckedFunction() ? 50 : 0) +
-                        /* (checkOutFormObj.formObject.isCodChecked ? 50 : 0) + */
-                        (
-                        cartList.cartItems.reduce((acc, items) => acc + items.qty * items.price, 0) > 500  
-                            ? 0
-                            : 50
-                        )
-                    }
-                </h6>
-                
-                </div>
-                <div style={{height:'65px'}} />
-                <div className='d-flex justify-content-center'>
-                    <button type='button' className='orderSummary-continue-shooping-btn' onClick={()=>{history.push('/')}}> {/*btn btn-outline-secondary continue-shopping-style */}
-                    Continue Shopping
-                    </button>
-                </div>
-                <div style={{height:'65px'}} />
-            </div>
-        );
-    }
-
-    
-    function isCodCheckedFunction(){
-        const itemPriceTemp = cartList.cartItems.reduce(
-            (acc, items) => acc + items.qty * items.price,
-            0
-        )
+  function isCodCheckedFunction() {
+    const itemPriceTemp = cartList.cartItems.reduce(
+      (acc, items) => acc + items.qty * items.price,
+      0
+    )
 
     if (checkOutFormObj.formObject.isCodChecked) {
       return itemPriceTemp > 500 ? true : false
@@ -188,4 +120,4 @@ export default function OrderSummaryCompT() {
 
     return itemPriceTemp < 500 ? true : false
   }
-
+}
