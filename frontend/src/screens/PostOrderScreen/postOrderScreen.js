d9325643b5792ce7f303f1b5e799e5e2d93effa5
zubae <afroze@outlook.in>
2021-08-27 15:47:31 +0530

    google ad conversion for all purchases

--- frontend/src/screens/PostOrderScreen/postOrderScreen.js
+++ frontend/src/screens/PostOrderScreen/postOrderScreen.js
@@ -3,21 +3,35 @@ import React from 'react'
 // import chinkyGirl from './assets/animeGirl.png'
 import './postOrderScreenStyle.css'
 import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 
 export default function PostOrderScreen() {
   return (
-    <div className='successBGimage'>
-      <div className='order-success-msg-wraper f-f-l'>
-        <div className='order-success-msg-container'>
-          <div>Thank you for choosing akiiko.</div>
-          <div>You will be receiving an email from us shortly.</div>
-        </div>
-        <div>
-          <Link to='/newprofile'>
-            <h6 className='continue-shooping-message'>View order status</h6>
-          </Link>
+    <>
+      <Helmet>
+        <title>Order Success</title>
+        <script>
+          {`gtag('event', 'conversion', {
+            'send_to': 'AW-322846655/rwSuCMaIge4CEL__-JkB',
+            'value': 1.0,
+            'currency': 'INR',
+            'transaction_id': ''
+        });`}
+        </script>
+      </Helmet>
+      <div className='successBGimage'>
+        <div className='order-success-msg-wraper f-f-l'>
+          <div className='order-success-msg-container'>
+            <div>Thank you for choosing akiiko.</div>
+            <div>You will be receiving an email from us shortly.</div>
+          </div>
+          <div>
+            <Link to='/newprofile'>
+              <h6 className='continue-shooping-message'>View order status</h6>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
