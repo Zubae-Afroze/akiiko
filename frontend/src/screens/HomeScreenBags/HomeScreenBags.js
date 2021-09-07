5b6a8f1bc319910492f3d1c40f18e4e66ccb639a
surya <samsurya336@gmail.com>
2021-09-07 16:03:19 +0530

    for data saving

--- frontend/src/screens/HomeScreenBags/HomeScreenBags.js
+++ frontend/src/screens/HomeScreenBags/HomeScreenBags.js
@@ -18,13 +18,13 @@ import './HomeScreenBags.css'
 import './NewHomeScreenBagsStyle.css'
 
 const HomeScreenBags = () => {
-  // const handleMouseEnter = (product) => {
-  //   document.getElementById(product.productId).src = product.hoverImage
-  // }
+  const handleMouseEnter = (product) => {
+    document.getElementById(product.productId).src = product.hoverImage
+  }
 
-  // const handleMouseOut = (product) => {
-  //   document.getElementById(product.productId).src = product.images[0]
-  // }
+  const handleMouseOut = (product) => {
+    document.getElementById(product.productId).src = product.images[0]
+  }
 
   //const [HomeScreenBagsProducts, setHomeScreenBagsProducts] = useState([]);
 
@@ -35,17 +35,18 @@ const HomeScreenBags = () => {
   //const { loading, error, products } = homeScreenBags
 
   const [bags, setBags] = useState([])
-  // const [loading, setLoading] = useState(false)
-  // const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
   const history = useHistory()
 
   useEffect(() => {
     //dispatch(actionListHomeBags())
-    //setLoading(true)
+    setLoading(true)
     axios
       .get('/api/home_screen_bags_products')
       .then((res) => {
         setBags(res.data)
+        setLoading(false)
       })
       .catch((err) => {
         console.log('HAS ERROR' + err)
@@ -56,7 +57,7 @@ const HomeScreenBags = () => {
 
   //const HomeScreenBagsProducts = []
 
-  return <NewComp />
+  return <OldComp />
 
   function NewComp() {
     return (
@@ -99,94 +100,94 @@ const HomeScreenBags = () => {
     )
   }
 
-  // function OldComp() {
-  //   return (
-  //     <div>
-  //       <Row className='women-wrapper'>
-  //         <Col sm={9} className='women-wrap'>
-  //           <h2 className='home-title women-title'>BAGS </h2>
-  //           <div className='women-text'>
-  //             A delightful range of sustainable bags that are designed to
-  //             perfection. Minimal, yet classic silhouettes to compliment just
-  //             about every look, multipurpose & eye-catching.
-  //           </div>
-  //         </Col>
-  //         <Col sm={3} className='women-button-placement'>
-  //           <Link to='/allproducts/productlist/bags'>
-  //             <button className='women-button'>SHOP ALL</button>
-  //           </Link>
-  //         </Col>
-  //       </Row>
-  //       {loading && (
-  //         <MyComponent
-  //           sentences={[]}
-  //           wrapperBackgroundColor={'rgba(255,255,255)'}
-  //           color={'#6e4e37'}
-  //           loaderType={'ball-spin-clockwise'}
-  //           customLoader={<SpinnerIcon />}
-  //         />
-  //       )}
-  //       {error && <h3>{error}</h3>}
-  //       {bags && (
-  //         <Row className='women-card-wrapper '>
-  //           {bags.map((product) => (
-  //             <div
-  //               className='women-shop-bag-container-root'
-  //               key={product.productId}
-  //             >
-  //               <Link to={`/product/${product._id}`}>
-  //                 <Col lg={2.6} className='women-shop-bag-container'>
-  //                   {/* <ProductItemComp /> */}
-  //                   <div>
-  //                     <div className='women-card-image'>
-  //                       <div className='women-img-wrap'>
-  //                         <img
-  //                           id={product.productId}
-  //                           src={product.heroImage}
-  //                           alt='women_1'
-  //                         />
-  //                         {product.bestSeller ? (
-  //                           <span className='label-best'>
-  //                             {product.bestSeller}
-  //                           </span>
-  //                         ) : null}
-  //                         {product.quickView ? (
-  //                           <span
-  //                             className='label-view'
-  //                             onMouseEnter={() => {
-  //                               handleMouseEnter(product)
-  //                             }}
-  //                             onMouseOut={() => {
-  //                               handleMouseOut(product)
-  //                             }}
-  //                           >
-  //                             {product.quickView}
-  //                           </span>
-  //                         ) : null}
-  //                       </div>
-  //                       <div className='women-card-title'>
-  //                         {product.productName}
-  //                       </div>
-  //                       <div className='women-card-text'>
-  //                         View Details - &#x20B9;
-  //                         {product.price ? product.price : product.mrpPrice}
-  //                       </div>
-  //                     </div>
-  //                   </div>
-  //                 </Col>
-  //               </Link>
-  //             </div>
-  //           ))}
-  //         </Row>
-  //       )}
-  //       <div className='hero-button mobile-view-button'>
-  //         <Link to='/allproducts/productlist/bags'>
-  //           <button>Shop Now</button>
-  //         </Link>
-  //       </div>
-  //     </div>
-  //   )
-  // }
+  function OldComp() {
+    return (
+      <div>
+        <Row className='women-wrapper'>
+          <Col sm={9} className='women-wrap'>
+            <h2 className='home-title women-title'>BAGS </h2>
+            <div className='women-text'>
+              A delightful range of sustainable bags that are designed to
+              perfection. Minimal, yet classic silhouettes to compliment just
+              about every look, multipurpose & eye-catching.
+            </div>
+          </Col>
+          <Col sm={3} className='women-button-placement'>
+            <Link to='/allproducts/productlist/bags'>
+              <button className='women-button'>SHOP ALL</button>
+            </Link>
+          </Col>
+        </Row>
+        {loading && (
+          <MyComponent
+            sentences={[]}
+            wrapperBackgroundColor={'rgba(255,255,255)'}
+            color={'#6e4e37'}
+            loaderType={'ball-spin-clockwise'}
+            customLoader={<SpinnerIcon />}
+          />
+        )}
+        {error && <h3>{error}</h3>}
+        {bags && (
+          <Row className='women-card-wrapper '>
+            {bags.map((product) => (
+              <div
+                className='women-shop-bag-container-root'
+                key={product.productId}
+              >
+                <Link to={`/product/${product._id}`}>
+                  <Col lg={2.6} className='women-shop-bag-container'>
+                    {/* <ProductItemComp /> */}
+                    <div>
+                      <div className='women-card-image'>
+                        <div className='women-img-wrap'>
+                          <img
+                            id={product.productId}
+                            src={product.heroImage}
+                            alt='women_1'
+                          />
+                          {product.bestSeller ? (
+                            <span className='label-best'>
+                              {product.bestSeller}
+                            </span>
+                          ) : null}
+                          {product.quickView ? (
+                            <span
+                              className='label-view'
+                              onMouseEnter={() => {
+                                handleMouseEnter(product)
+                              }}
+                              onMouseOut={() => {
+                                handleMouseOut(product)
+                              }}
+                            >
+                              {product.quickView}
+                            </span>
+                          ) : null}
+                        </div>
+                        <div className='women-card-title'>
+                          {product.productName}
+                        </div>
+                        <div className='women-card-text'>
+                          View Details - &#x20B9;
+                          {product.price ? product.price : product.mrpPrice}
+                        </div>
+                      </div>
+                    </div>
+                  </Col>
+                </Link>
+              </div>
+            ))}
+          </Row>
+        )}
+        <div className='hero-button mobile-view-button'>
+          <Link to='/allproducts/productlist/bags'>
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default HomeScreenBags
--- frontend/src/screens/HomeScreenHome/HomeScreenHome.js
+++ frontend/src/screens/HomeScreenHome/HomeScreenHome.js
@@ -15,13 +15,13 @@ import './NewHomeScreenHomeStyle.css'
 //import HomeScreenHomeProducts from '../../assets/products/HomeScreenHomeProducts';
 
 const HomeScreenHome = () => {
-  // const handleMouseEnter = (product) => {
-  //   document.getElementById(product.productId).src = product.hoverImage
-  // }
+  const handleMouseEnter = (product) => {
+    document.getElementById(product.productId).src = product.hoverImage
+  }
 
-  // const handleMouseOut = (product) => {
-  //   document.getElementById(product.productId).src = product.images[0]
-  // }
+  const handleMouseOut = (product) => {
+    document.getElementById(product.productId).src = product.images[0]
+  }
 
   //const [HomeScreenHomeProducts, setHomeScreenHomeProducts] = useState([]);
 
@@ -38,11 +38,11 @@ const HomeScreenHome = () => {
   const history = useHistory()
 
   useEffect(() => {
-    // setLoading(true)
+    setLoading(true)
     axios
       .get('/api/home_screen_home_products')
       .then((res) => {
-        // setLoading(false)
+        setLoading(false)
         setHome(res.data)
         // setError('')
       })
@@ -56,7 +56,7 @@ const HomeScreenHome = () => {
 
   return (
     <>
-      <NewComp />
+      <OldComp />
     </>
   )
 
@@ -116,99 +116,99 @@ const HomeScreenHome = () => {
     )
   }
 
-  // function OldComp() {
-  //   return (
-  //     <div className='home-screen-component'>
-  //       <Row className='home-wrapper workout-cont'>
-  //         <Col sm={3} className='home-button-placement'>
-  //           <Link to='/allproducts/productlist/home'>
-  //             <button className='home-button'>SHOP ALL</button>
-  //           </Link>
-  //         </Col>
-  //         <Col sm={9} className='home-wrapper'>
-  //           <h2 className='home-title'>HOME</h2>
-  //           <div className='home-text'>
-  //             A simple yet inviting collection of natural fabric kitchen
-  //             accessories that will blend harmoniously with every home’s mood &
-  //             feel. Multi-purpose & vegan lunch bags, aprons & more
-  //           </div>
-  //         </Col>
-  //       </Row>
-  //       <Row>
-  //         <Col sm={5} className='hero-image-workout'>
-  //           <div className='workout-hero-image'>
-  //             <img
-  //               src={`/images/home_screen_images/HomeScreenHomeHero.jpg`}
-  //               alt='home_hero_img'
-  //             />
-  //           </div>
-  //         </Col>
-  //         {loading && (
-  //           <MyComponent
-  //             sentences={[]}
-  //             wrapperBackgroundColor={'rgba(255,255,255)'}
-  //             color={'#6e4e37'}
-  //             loaderType={'ball-spin-clockwise'}
-  //             customLoader={<SpinnerIcon />}
-  //           />
-  //         )}
-  //         {error && <h3>{error}</h3>}
-  //         {home && (
-  //           <Col sm={7} className='workout-container'>
-  //             <Row className='workout-card-wrapper'>
-  //               {home.map((product) => (
-  //                 <Col className='home-card-items' key={product._id}>
-  //                   <Link to={`/product/${product._id}`}>
-  //                     <div>
-  //                       <div className='home-img-wrap'>
-  //                         <img
-  //                           id={product.productId}
-  //                           className='home-card-image'
-  //                           src={product.images[0]}
-  //                           alt='home_1'
-  //                         />
-  //                         {product.bestSeller ? (
-  //                           <span className='label-best label-best-workout'>
-  //                             {product.bestSeller}
-  //                           </span>
-  //                         ) : null}
-  //                         {product.quickView ? (
-  //                           <span
-  //                             className='label-view label-view-workout'
-  //                             onMouseEnter={() => {
-  //                               handleMouseEnter(product)
-  //                             }}
-  //                             onMouseOut={() => {
-  //                               handleMouseOut(product)
-  //                             }}
-  //                           >
-  //                             {product.quickView}
-  //                           </span>
-  //                         ) : null}
-  //                       </div>
-  //                       <div className='home-card-title'>
-  //                         {product.productName}
-  //                       </div>
-  //                       <div className='home-card-text'>
-  //                         View Details - &#x20B9;
-  //                         {product.price ? product.price : product.mrpPrice}
-  //                       </div>
-  //                     </div>
-  //                   </Link>
-  //                 </Col>
-  //               ))}
-  //             </Row>
-  //           </Col>
-  //         )}
-  //       </Row>
-  //       <div className='hero-button mobile-view-button'>
-  //         <Link to='/home/all_products'>
-  //           <button>Shop Now</button>
-  //         </Link>
-  //       </div>
-  //     </div>
-  //   )
-  // }
+  function OldComp() {
+    return (
+      <div className='home-screen-component'>
+        <Row className='home-wrapper workout-cont'>
+          <Col sm={3} className='home-button-placement'>
+            <Link to='/allproducts/productlist/home'>
+              <button className='home-button'>SHOP ALL</button>
+            </Link>
+          </Col>
+          <Col sm={9} className='home-wrapper'>
+            <h2 className='home-title'>HOME</h2>
+            <div className='home-text'>
+              A simple yet inviting collection of natural fabric kitchen
+              accessories that will blend harmoniously with every home’s mood &
+              feel. Multi-purpose & vegan lunch bags, aprons & more
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={5} className='hero-image-workout'>
+            <div className='workout-hero-image'>
+              <img
+                src={`/images/home_screen_images/HomeScreenHomeHero.jpg`}
+                alt='home_hero_img'
+              />
+            </div>
+          </Col>
+          {loading && (
+            <MyComponent
+              sentences={[]}
+              wrapperBackgroundColor={'rgba(255,255,255)'}
+              color={'#6e4e37'}
+              loaderType={'ball-spin-clockwise'}
+              customLoader={<SpinnerIcon />}
+            />
+          )}
+          {error && <h3>{error}</h3>}
+          {home && (
+            <Col sm={7} className='workout-container'>
+              <Row className='workout-card-wrapper'>
+                {home.map((product) => (
+                  <Col className='home-card-items' key={product._id}>
+                    <Link to={`/product/${product._id}`}>
+                      <div>
+                        <div className='home-img-wrap'>
+                          <img
+                            id={product.productId}
+                            className='home-card-image'
+                            src={product.images[0]}
+                            alt='home_1'
+                          />
+                          {product.bestSeller ? (
+                            <span className='label-best label-best-workout'>
+                              {product.bestSeller}
+                            </span>
+                          ) : null}
+                          {product.quickView ? (
+                            <span
+                              className='label-view label-view-workout'
+                              onMouseEnter={() => {
+                                handleMouseEnter(product)
+                              }}
+                              onMouseOut={() => {
+                                handleMouseOut(product)
+                              }}
+                            >
+                              {product.quickView}
+                            </span>
+                          ) : null}
+                        </div>
+                        <div className='home-card-title'>
+                          {product.productName}
+                        </div>
+                        <div className='home-card-text'>
+                          View Details - &#x20B9;
+                          {product.price ? product.price : product.mrpPrice}
+                        </div>
+                      </div>
+                    </Link>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
+          )}
+        </Row>
+        <div className='hero-button mobile-view-button'>
+          <Link to='/home/all_products'>
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default HomeScreenHome
