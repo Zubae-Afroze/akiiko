e0978c86d0867e1257c566e5fa8193cd883970f6
zubae <zubae@live.in>
2021-09-15 17:23:49 +0530

    force HTTPS, postOrderScreen

--- frontend/src/screens/PostOrderScreen/postOrderScreen.js
+++ frontend/src/screens/PostOrderScreen/postOrderScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect , useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // import { Row, Col } from 'react-bootstrap'
 // import chinkyGirl from './assets/animeGirl.png'
 import './postOrderScreenStyle.css'
@@ -14,7 +14,7 @@ export default function PostOrderScreen() {
 
   let finalOrder = {}
 
-  const [ openThanksNote , setOpenThanksNote ]= useState(false)
+  const [openThanksNote, setOpenThanksNote] = useState(false)
 
   if (order) {
     finalOrder = order
@@ -41,10 +41,8 @@ export default function PostOrderScreen() {
         </script>
       </Helmet>
 
-
-      
       <div className='successBGimage'>
-        {/* <div className='order-success-msg-wraper f-f-l'>
+        <div className='order-success-msg-wraper f-f-l'>
           <div className='order-success-msg-container'>
             <div>Thank you for choosing akiiko.</div>
             <div>You will be receiving an email from us shortly.</div>
@@ -54,10 +52,10 @@ export default function PostOrderScreen() {
               <h6 className='continue-shooping-message'>View order status</h6>
             </Link>
           </div>
-        </div> */}
+        </div>
 
-        <div className='feedback-form-pop-up-wrapper'>
-          <div className="feedback-form-msg-container">
+        {/* <div className='feedback-form-pop-up-wrapper'>
+          <div className='feedback-form-msg-container'>
             <div>Thank you for choosing akiiko.</div>
             <div>You will be receiving an email from us shortly.</div>
           </div>
@@ -65,139 +63,196 @@ export default function PostOrderScreen() {
           <FeedBackFormRattingComp />
 
           <div>
-            <textarea id="story" placeholder="Tell us more" name="message" class="feedBackArea"></textarea>
+            <textarea
+              id='story'
+              placeholder='Tell us more'
+              name='message'
+              class='feedBackArea'
+            ></textarea>
           </div>
 
-          <div className="form-submit-buttons-container">
-            <button 
-              onClick={()=>{
-                setOpenThanksNote(true);
+          <div className='form-submit-buttons-container'>
+            <button
+              onClick={() => {
+                setOpenThanksNote(true)
                 // console.log(openThanksNote.current)
               }}
-            >submmit</button>
+            >
+              submmit
+            </button>
             <button>skip</button>
           </div>
 
-          <input type="checkbox" id="toggle" style={{display:'none'}} checked={openThanksNote} 
-
-            onChange={(e)=>{
+          <input
+            type='checkbox'
+            id='toggle'
+            style={{ display: 'none' }}
+            checked={openThanksNote}
+            onChange={(e) => {
               setOpenThanksNote(e.target.checked)
             }}
-
-          />
-          {/* 
+          /> */}
+        {/* 
           position: "absolute", bottom: "2px", zIndex: 2
           
           <button style={{position: "absolute", bottom: "2px",left: "5px", zIndex: 3}} onClick={()=>{
             const elem = document.querySelector(".feedback-form-pop-up-wrapper");
             elem.style.animation = "feedbackFormOuttro 0.5s 0s";
           }} > scale out </button> */}
-          
-          <div className="feedback-thanks-wrapper">
+
+        {/* <div className='feedback-thanks-wrapper'>
             Thanks for sharing your experience with us.
           </div>
-
-        </div>
+        </div> */}
       </div>
     </>
   )
-
-
-
 }
 
-
-
-
-
-function FeedBackFormRattingComp(){
-
-  const [rating , setRating ] = React.useState(-1)
-  const [ stars, setStars ] = React.useState([
-    unFilledstar,unFilledstar,unFilledstar,unFilledstar,unFilledstar
+function FeedBackFormRattingComp() {
+  const [rating, setRating] = React.useState(-1)
+  const [stars, setStars] = React.useState([
+    unFilledstar,
+    unFilledstar,
+    unFilledstar,
+    unFilledstar,
+    unFilledstar,
   ])
 
   const changeRating = (val) => {
-    let newStars = [];
-    stars.forEach((_,index) => {
-      if(index <= val){
+    let newStars = []
+    stars.forEach((_, index) => {
+      if (index <= val) {
         newStars.push(filledstar)
-      }else{
+      } else {
         newStars.push(unFilledstar)
       }
-    });
+    })
     setStars(newStars)
 
-    const element = document.getElementById("rc");
-    element.classList.remove("reactions-container");
-    void element.offsetWidth;
-    element.classList.add("reactions-container");
+    const element = document.getElementById('rc')
+    element.classList.remove('reactions-container')
+    void element.offsetWidth
+    element.classList.add('reactions-container')
     setTimeout(() => {
       setRating(val)
-    }, 300);
+    }, 300)
   }
 
   const onHoverStar = (val) => {
-    let newStars = [];
-    stars.forEach((_,index) => {
-      if(index <= val){
+    let newStars = []
+    stars.forEach((_, index) => {
+      if (index <= val) {
         newStars.push(filledstar)
-      }else{
+      } else {
         newStars.push(unFilledstar)
       }
-    });
+    })
     setStars(newStars)
   }
 
   const onLevaeStars = () => {
-    if(rating > -1){
-
-      let newStars = [];
-      stars.forEach((_,index) => {
-        if(index <= rating){
+    if (rating > -1) {
+      let newStars = []
+      stars.forEach((_, index) => {
+        if (index <= rating) {
           newStars.push(filledstar)
-        }else{
+        } else {
           newStars.push(unFilledstar)
         }
-      });
+      })
       setStars(newStars)
 
       // setStars([unFilledstar,unFilledstar,unFilledstar,unFilledstar,unFilledstar])
-    }else{
-      setStars([unFilledstar,unFilledstar,unFilledstar,unFilledstar,unFilledstar])
+    } else {
+      setStars([
+        unFilledstar,
+        unFilledstar,
+        unFilledstar,
+        unFilledstar,
+        unFilledstar,
+      ])
     }
   }
 
-  return(
-    <div className="feedback-form-ratting-container">
+  return (
+    <div className='feedback-form-ratting-container'>
       <div>How would you rate your experience from our site ?</div>
-      <div className="reactions-container-wrapper">
-          <div id="rc" className="reactions-container">
-            {rating + 1}
-          </div>
+      <div className='reactions-container-wrapper'>
+        <div id='rc' className='reactions-container'>
+          {rating + 1}
+        </div>
       </div>
 
-      <div className="rating-stars" 
-      // onClick={()=>{
-      //   // const elem = document.querySelector(".reactions-container");
+      <div
+        className='rating-stars'
+        // onClick={()=>{
+        //   // const elem = document.querySelector(".reactions-container");
 
-      //   const element = document.getElementById("rc")
-      //   // transition: 0.5s;
-      //   // animation: feedbackFormIntro 0.8s;
-      //   element.classList.remove("reactions-container");
-      //   void element.offsetWidth;
-      //   element.classList.add("reactions-container")
-      //   // elem.style.animation = "none"
-      //   // elem.style.animation = "reactionsContainerAnimation 0.5s 0s";
-      // }}
-      onMouseLeave={()=>{ onLevaeStars() }}
+        //   const element = document.getElementById("rc")
+        //   // transition: 0.5s;
+        //   // animation: feedbackFormIntro 0.8s;
+        //   element.classList.remove("reactions-container");
+        //   void element.offsetWidth;
+        //   element.classList.add("reactions-container")
+        //   // elem.style.animation = "none"
+        //   // elem.style.animation = "reactionsContainerAnimation 0.5s 0s";
+        // }}
+        onMouseLeave={() => {
+          onLevaeStars()
+        }}
       >
-        <img src={stars[0]} alt="" onClick={()=>{ changeRating(0) }} onMouseEnter={()=>{onHoverStar(0)}} />
-        <img src={stars[1]} alt="" onClick={()=>{ changeRating(1) }} onMouseEnter={()=>{onHoverStar(1)}} />
-        <img src={stars[2]} alt="" onClick={()=>{ changeRating(2) }} onMouseEnter={()=>{onHoverStar(2)}} />
-        <img src={stars[3]} alt="" onClick={()=>{ changeRating(3) }} onMouseEnter={()=>{onHoverStar(3)}} />
-        <img src={stars[4]} alt="" onClick={()=>{ changeRating(4) }} onMouseEnter={()=>{onHoverStar(4)}} />
+        <img
+          src={stars[0]}
+          alt=''
+          onClick={() => {
+            changeRating(0)
+          }}
+          onMouseEnter={() => {
+            onHoverStar(0)
+          }}
+        />
+        <img
+          src={stars[1]}
+          alt=''
+          onClick={() => {
+            changeRating(1)
+          }}
+          onMouseEnter={() => {
+            onHoverStar(1)
+          }}
+        />
+        <img
+          src={stars[2]}
+          alt=''
+          onClick={() => {
+            changeRating(2)
+          }}
+          onMouseEnter={() => {
+            onHoverStar(2)
+          }}
+        />
+        <img
+          src={stars[3]}
+          alt=''
+          onClick={() => {
+            changeRating(3)
+          }}
+          onMouseEnter={() => {
+            onHoverStar(3)
+          }}
+        />
+        <img
+          src={stars[4]}
+          alt=''
+          onClick={() => {
+            changeRating(4)
+          }}
+          onMouseEnter={() => {
+            onHoverStar(4)
+          }}
+        />
       </div>
     </div>
-  );
+  )
 }
