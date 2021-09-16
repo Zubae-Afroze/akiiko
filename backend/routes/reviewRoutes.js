57412f4ad4094ce062f0d4e2742b5c7fe11e96c9
zubae <zubae@live.in>
2021-09-16 16:51:55 +0530

    review conroller import

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import { createReview } from '../controllers/reviewController.js'
 
 const router = express.Router()
 
-router.route('/post').post()
+router.route('/post').post(createReview)
 
 export default router
