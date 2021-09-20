a4e7d70c7b6fe0d99e6fe264736b4aa3d727bf59
zubae <zubae@live.in>
2021-09-20 12:33:32 +0530

    test screen alignment

--- frontend/src/screens/TestScreen/TestScreen.js
+++ frontend/src/screens/TestScreen/TestScreen.js
@@ -8,10 +8,10 @@ import './TestScreen.css'
 import { Container } from 'react-bootstrap'
 import ProductTagComponent from '../../components/ProductTagComp/ProductTagComp'
 import CarouselItem from '../../components/CarouselItemComp/CarouselItem'
-import desktopImg from './Desktop.jpg';
-import tabImg from './Ipad.jpg';
-import mob from './Mobile.jpg';
-import {  Carousel } from 'react-bootstrap'
+import desktopImg from './Desktop.jpg'
+import tabImg from './Ipad.jpg'
+import mob from './Mobile.jpg'
+import { Carousel } from 'react-bootstrap'
 
 // import { storage } from '../..'
 
@@ -26,64 +26,153 @@ const TestScreen = () => {
   //     setImageRef(url)
   //     return url
   //   })
-  const intervalVal = 6000;
+  const intervalVal = 6000
 
   return (
     <>
-    {/* <CarouselItem urls={[desktopImg,desktopImg,tabImg,mob]}>
+      {/* <CarouselItem urls={[desktopImg,desktopImg,tabImg,mob]}>
           <ProductTagComponent laptop={['18%','40%']} tab={['15%','40%']} mob={['12%','40%']} title={'Product 1'} price={'100'} />
           <ProductTagComponent laptop={['30%','52%']} tab={['23%','45%']} mob={['18%','50%']} title={'Product 2'} price={'1200'} />
           <ProductTagComponent laptop={['13%','80%']} tab={['13%','72%']} mob={['12%','80%']} title={'Product 3'} price={'999'} />
         </CarouselItem> */}
-    <Carousel pause={'hover'} controls={false}>
+      <Carousel pause={'hover'} controls={false}>
+        <Carousel.Item interval={intervalVal}>
+          <CarouselItem urls={[desktopImg, desktopImg, tabImg, mob]}>
+            <ProductTagComponent
+              laptop={['18%', '40%']}
+              tab={['15%', '40%']}
+              mob={['12%', '40%']}
+              title={'Product 1'}
+              price={'100'}
+            />
+            <ProductTagComponent
+              laptop={['30%', '52%']}
+              tab={['23%', '45%']}
+              mob={['18%', '50%']}
+              title={'Product 2'}
+              price={'1200'}
+            />
+            <ProductTagComponent
+              laptop={['13%', '80%']}
+              tab={['13%', '72%']}
+              mob={['12%', '80%']}
+              title={'Product 3'}
+              price={'999'}
+            />
+          </CarouselItem>
+        </Carousel.Item>
 
-      <Carousel.Item interval={intervalVal}>
-        <CarouselItem urls={[desktopImg,desktopImg,tabImg,mob]}>
-          <ProductTagComponent laptop={['18%','40%']} tab={['15%','40%']} mob={['12%','40%']} title={'Product 1'} price={'100'} />
-          <ProductTagComponent laptop={['30%','52%']} tab={['23%','45%']} mob={['18%','50%']} title={'Product 2'} price={'1200'} />
-          <ProductTagComponent laptop={['13%','80%']} tab={['13%','72%']} mob={['12%','80%']} title={'Product 3'} price={'999'} />
-        </CarouselItem>
-      </Carousel.Item>
+        <Carousel.Item interval={intervalVal}>
+          <CarouselItem urls={[desktopImg, desktopImg, tabImg, mob]}>
+            <ProductTagComponent
+              laptop={['18%', '40%']}
+              tab={['15%', '40%']}
+              mob={['12%', '40%']}
+              title={'Product 1'}
+              price={'100'}
+            />
+            <ProductTagComponent
+              laptop={['30%', '52%']}
+              tab={['23%', '45%']}
+              mob={['18%', '50%']}
+              title={'Product 2'}
+              price={'1200'}
+            />
+            <ProductTagComponent
+              laptop={['13%', '80%']}
+              tab={['13%', '72%']}
+              mob={['12%', '80%']}
+              title={'Product 3'}
+              price={'999'}
+            />
+          </CarouselItem>
+          <Carousel.Caption className='carousel-slide-2'></Carousel.Caption>
+        </Carousel.Item>
 
-      <Carousel.Item interval={intervalVal}>
-      <CarouselItem urls={[desktopImg,desktopImg,tabImg,mob]}>
-          <ProductTagComponent laptop={['18%','40%']} tab={['15%','40%']} mob={['12%','40%']} title={'Product 1'} price={'100'} />
-          <ProductTagComponent laptop={['30%','52%']} tab={['23%','45%']} mob={['18%','50%']} title={'Product 2'} price={'1200'} />
-          <ProductTagComponent laptop={['13%','80%']} tab={['13%','72%']} mob={['12%','80%']} title={'Product 3'} price={'999'} />
-        </CarouselItem>
-        <Carousel.Caption className='carousel-slide-2'></Carousel.Caption>
-      </Carousel.Item>
+        <Carousel.Item interval={intervalVal}>
+          <CarouselItem urls={[desktopImg, desktopImg, tabImg, mob]}>
+            <ProductTagComponent
+              laptop={['18%', '40%']}
+              tab={['15%', '40%']}
+              mob={['12%', '40%']}
+              title={'Product 1'}
+              price={'100'}
+            />
+            <ProductTagComponent
+              laptop={['30%', '52%']}
+              tab={['23%', '45%']}
+              mob={['18%', '50%']}
+              title={'Product 2'}
+              price={'1200'}
+            />
+            <ProductTagComponent
+              laptop={['13%', '80%']}
+              tab={['13%', '72%']}
+              mob={['12%', '80%']}
+              title={'Product 3'}
+              price={'999'}
+            />
+          </CarouselItem>
+          <Carousel.Caption className='carousel-slide-3'></Carousel.Caption>
+        </Carousel.Item>
 
-      <Carousel.Item interval={intervalVal}>
-        <CarouselItem urls={[desktopImg,desktopImg,tabImg,mob]}>
-          <ProductTagComponent laptop={['18%','40%']} tab={['15%','40%']} mob={['12%','40%']} title={'Product 1'} price={'100'} />
-          <ProductTagComponent laptop={['30%','52%']} tab={['23%','45%']} mob={['18%','50%']} title={'Product 2'} price={'1200'} />
-          <ProductTagComponent laptop={['13%','80%']} tab={['13%','72%']} mob={['12%','80%']} title={'Product 3'} price={'999'} />
-        </CarouselItem>
-        <Carousel.Caption className='carousel-slide-3'></Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={intervalVal}>
-        <CarouselItem urls={[desktopImg,desktopImg,tabImg,mob]}>
-          <ProductTagComponent laptop={['18%','40%']} tab={['15%','40%']} mob={['12%','40%']} title={'Product 1'} price={'100'} />
-          <ProductTagComponent laptop={['30%','52%']} tab={['23%','45%']} mob={['18%','50%']} title={'Product 2'} price={'1200'} />
-          <ProductTagComponent laptop={['13%','80%']} tab={['13%','72%']} mob={['12%','80%']} title={'Product 3'} price={'999'} />
-        </CarouselItem>
-      </Carousel.Item>
-    </Carousel>
+        <Carousel.Item interval={intervalVal}>
+          <CarouselItem urls={[desktopImg, desktopImg, tabImg, mob]}>
+            <ProductTagComponent
+              laptop={['18%', '40%']}
+              tab={['15%', '40%']}
+              mob={['12%', '40%']}
+              title={'Product 1'}
+              price={'100'}
+            />
+            <ProductTagComponent
+              laptop={['30%', '52%']}
+              tab={['23%', '45%']}
+              mob={['18%', '50%']}
+              title={'Product 2'}
+              price={'1200'}
+            />
+            <ProductTagComponent
+              laptop={['13%', '80%']}
+              tab={['13%', '72%']}
+              mob={['12%', '80%']}
+              title={'Product 3'}
+              price={'999'}
+            />
+          </CarouselItem>
+        </Carousel.Item>
+      </Carousel>
     </>
   )
 }
 
-function TestComp(){
+function TestComp() {
   return (
     <>
       <div className='test__banner-image'>
         {/* <img src={'/images/carousel_images/new_banner.jpg'} alt='image_ref' /> */}
         {/* <ProductTagComp /> */}
-        <ProductTagComponent laptop={['18%','40%']} tab={['15%','40%']} mob={['12%','40%']} title={'Product 1'} price={'100'} />
-        <ProductTagComponent laptop={['30%','52%']} tab={['23%','45%']} mob={['18%','50%']} title={'Product 2'} price={'1200'} />
-        <ProductTagComponent laptop={['13%','80%']} tab={['13%','72%']} mob={['12%','80%']} title={'Product 3'} price={'999'} />
+        <ProductTagComponent
+          laptop={['18%', '40%']}
+          tab={['15%', '40%']}
+          mob={['12%', '40%']}
+          title={'Product 1'}
+          price={'100'}
+        />
+        <ProductTagComponent
+          laptop={['30%', '52%']}
+          tab={['23%', '45%']}
+          mob={['18%', '50%']}
+          title={'Product 2'}
+          price={'1200'}
+        />
+        <ProductTagComponent
+          laptop={['13%', '80%']}
+          tab={['13%', '72%']}
+          mob={['12%', '80%']}
+          title={'Product 3'}
+          price={'999'}
+        />
       </div>
       <Container>
         <HomeScreenBags />
@@ -95,27 +184,24 @@ function TestComp(){
   )
 }
 
-
-function ProductTagComp(){
-  return(
+function ProductTagComp() {
+  return (
     <>
-    {/* <div className='hover_box' /> */}
-    <div className='product_tag'>
-      <div className='product_detail'>
-        <p>Product Title kisudyerhq asioudyaoiws </p>
-        <Link
-          to={'/product/5fe18171a384156e680756fe'}
-        >
-        <div>&#x20B9; 99</div>
-        <div className='view-product-tag-btn'>View Product &#10095;</div>
-        </Link>
+      {/* <div className='hover_box' /> */}
+      <div className='product_tag'>
+        <div className='product_detail'>
+          <p>Product Title kisudyerhq asioudyaoiws </p>
+          <Link to={'/product/5fe18171a384156e680756fe'}>
+            <div>&#x20B9; 99</div>
+            <div className='view-product-tag-btn'>View Product &#10095;</div>
+          </Link>
+        </div>
       </div>
-    </div>
-    {/* <div className='product_tag_detail_box'>
+      {/* <div className='product_tag_detail_box'>
       price - 99
     </div> */}
     </>
-  );
+  )
 }
 
 export default TestScreen
