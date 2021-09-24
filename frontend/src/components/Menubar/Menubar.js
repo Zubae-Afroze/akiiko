4013e2064e7b0ca43fb5d42a2030606fdceecd90
zubae <zubae@live.in>
2021-09-24 16:21:53 +0530

    new change

--- frontend/src/components/Menubar/Menubar.js
+++ frontend/src/components/Menubar/Menubar.js
@@ -20,9 +20,7 @@ export default class Menubar extends Component {
   }
 
   handleScroll = () => {
-    const prevScrollPos = this.state.prevScrollPos;
-
-    console.log(prevScrollPos)
+    const prevScrollPos = this.state.prevScrollPos
 
     const currentScrollPos = window.pageYOffset
     const navFixed = prevScrollPos < currentScrollPos
@@ -32,7 +30,7 @@ export default class Menubar extends Component {
       navFixed,
     })
   }
- 
+
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll)
   }
