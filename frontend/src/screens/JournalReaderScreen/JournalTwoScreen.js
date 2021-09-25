d11383d5a60bcaaf8ee1f69380b52be6d88b9cef
zubae <zubae@live.in>
2021-09-25 18:17:36 +0530

    editted journal 2

--- frontend/src/screens/JournalReaderScreen/JournalTwoScreen.js
+++ frontend/src/screens/JournalReaderScreen/JournalTwoScreen.js
@@ -17,8 +17,10 @@ const JournalTwoScreen = () => {
           <div className='jone_main'>
             <div className='jone_alt'>
               <h2>
-              HOW TO: <br />
-                <span className='alt_color'>Set up a Nature Inspired Table</span>
+                HOW TO: <br />
+                <span className='alt_color'>
+                  Set up a Nature Inspired Table
+                </span>
               </h2>
             </div>
             <div className='jone_author-div'>
@@ -31,69 +33,97 @@ const JournalTwoScreen = () => {
             </div>
             <div div className='jone_div-wrap'>
               <p>
-              If the heart of a house lies in the kitchen, the soul may definitely reside on the dining table of a
-household. Dining rooms’ trends tend to evolve with them, but nothing can match something
-minimal, classic and timeless. The art of “less is more” can be very artfully depicted on a nature
-inspired dining table. 
-
+                If the heart of a house lies in the kitchen, the soul may
+                definitely reside on the dining table of a household. Dining
+                rooms’ trends tend to evolve with them, but nothing can match
+                something minimal, classic and timeless. The art of “less is
+                more” can be very artfully depicted on a nature inspired dining
+                table.
               </p>
-              <br/>
+              <br />
               <p>
-              Natural elements include the usage of organic products in a tasteful manner. In order to make a
-space attractive yet multifunctional, one has to carefully pick the kind of storage ideas that are
-considered for a table. For example, Akiiko’s Dining Range consists of items that a dining table
-of the modern home needs. Equipped with a set of circle and square trays with a detachable
-wooden stand, placemats, striped coasters and table runners, Akiiko’s idea towards dining ware
-is rather simplistic, but great attention is given towards usability. 
-
+                Natural elements include the usage of organic products in a
+                tasteful manner. In order to make a space attractive yet
+                multifunctional, one has to carefully pick the kind of storage
+                ideas that are considered for a table. For example, Akiiko’s
+                Dining Range consists of items that a dining table of the modern
+                home needs. Equipped with a set of{' '}
+                <Link to='/product/circle-bread-baskets-with-wooden-stand'>
+                  circle
+                </Link>{' '}
+                and{' '}
+                <Link to='/product/square-bread-baskets-with-wooden-stand'>
+                  square
+                </Link>
+                trays with a detachable wooden stand,{' '}
+                <Link to='/product/biskk-placemats'>placemats</Link>,{' '}
+                <Link to='/product/square-stripe-coasters'>
+                  striped coasters
+                </Link>
+                and <Link to='/product/herra-tablerunner'>table runners</Link>,
+                Akiiko’s idea towards dining ware is rather simplistic, but
+                great attention is given towards usability.
               </p>
-              <br/>
+              <br />
               <div className='jone_img'>
                 <img src='/images/blog_images_2/1.jpg' alt='jone_banner' />
                 <img src='/images/blog_images_2/2.jpg' alt='jone_banner' />
               </div>
               <p>
-              In order to understand how to incorporate simple eco-friendly elements for your dining table,
-consider the following points: 
+                In order to understand how to incorporate simple eco-friendly
+                elements for your dining table, consider the following points:
               </p>
             </div>
             <div className='jone_div-wrap'>
               <h3>1. Always go with a neutral colour palette:</h3>
               <p>
-              Going neutral always works its magic when we want something timeless and elegant. Hence,
-choosing neutral hues will always be preferable. 
-              </p>
-              <br/>
-              <div className='jone_banner-img'>
-              <img src='/images/blog_images_2/3.jpg' alt='jone_banner' />
-            </div>
-              <p>
-              Akiiko’s “Biskk Coasters” come in light shades that compliment the space and make it look
-appealing. It’s also important to consider the natural fabric that is used on the table. Akiiko’s
-“Table Runners” are crafted out of three major fabrics; Woven cotton, Juco and Hemp. These
-fabrics are extremely airy and durable. 
-              </p>
-            </div>
-            <div className='jone_div-wrap'>
-              <h3>
-              2. Incorporate quirky elements of storage: 
-              </h3>
-              <p>
-              Storage plays a vital role in a decluttered space. Consider storing your bread and essentials in
-small storage boxes which also enhance the overall appeal of the space.
+                Going neutral always works its magic when we want something
+                timeless and elegant. Hence, choosing neutral hues will always
+                be preferable.
               </p>
               <br />
               <div className='jone_banner-img'>
-              <img src='/images/blog_images_2/4.jpg' alt='jone_banner' />
-            </div>
+                <img src='/images/blog_images_2/3.jpg' alt='jone_banner' />
+              </div>
               <p>
-              Akiiko has introduced “Circle Bread Baskets'' which come with a detachable wooden stand.
-These baskets can be used to keep bread or some other forms of grub on the table. Some
-vegan snacks, such as trail mixes, crackers and roasted chickpeas can also be artfully
-displayed on Akiiko’s circle and square trays. These utilitarian baskets and trays are pleasing to
-look at and highly effective too. With a shift in the eating habits of people, veganism as a way of
-life is taking momentum steadily. Vegans have always had a strong take on classic sandwiches.
-Akiiko’s sandwich bags are practical, eye-catching and ensure garden-freshness all day long! 
+                Akiiko’s{' '}
+                <Link to='/product/biskk-coasters'>“Biskk Coasters”</Link> come
+                in light shades that compliment the space and make it look
+                appealing. It’s also important to consider the natural fabric
+                that is used on the table. Akiiko’s{' '}
+                <Link to='/product/fringe-hemp-tableRunner'>
+                  “Table Runners”
+                </Link>{' '}
+                are crafted out of three major fabrics; Woven cotton, Juco and
+                Hemp. These fabrics are extremely airy and durable.
+              </p>
+            </div>
+            <div className='jone_div-wrap'>
+              <h3>2. Incorporate quirky elements of storage:</h3>
+              <p>
+                Storage plays a vital role in a decluttered space. Consider
+                storing your bread and essentials in small storage boxes which
+                also enhance the overall appeal of the space.
+              </p>
+              <br />
+              <div className='jone_banner-img'>
+                <img src='/images/blog_images_2/4.jpg' alt='jone_banner' />
+              </div>
+              <p>
+                Akiiko has introduced{' '}
+                <Link to='/product/circle-bread-baskets-with-wooden-stand'>
+                  “Circle Bread Baskets"
+                </Link>{' '}
+                which come with a detachable wooden stand. These baskets can be
+                used to keep bread or some other forms of grub on the table.
+                Some vegan snacks, such as trail mixes, crackers and roasted
+                chickpeas can also be artfully displayed on Akiiko’s circle and
+                square trays. These utilitarian baskets and trays are pleasing
+                to look at and highly effective too. With a shift in the eating
+                habits of people, veganism as a way of life is taking momentum
+                steadily. Vegans have always had a strong take on classic
+                sandwiches. Akiiko’s sandwich bags are practical, eye-catching
+                and ensure garden-freshness all day long!
               </p>
             </div>
             {/* <div className='jone_img'>
@@ -103,11 +133,13 @@ Akiiko’s sandwich bags are practical, eye-catching and ensure garden-freshness
             <div className='jone_div-wrap'>
               <h3>3. Always pay heed to the tiny details: </h3>
               <p>
-              A well-suited dining table is a combination of all things big and small, and the small ones take
-up most of the space in actuality. Hence, consider displaying your cutlery in a fashionable way
-by using Akiiko’s cutlery case which comes in a pack of six. This vegan fabric cutlery case is
-perfect when you’re packing your lunch or simply making a statement with your food. They also
-ensure that the cutlery remains free of dust. 
+                A well-suited dining table is a combination of all things big
+                and small, and the small ones take up most of the space in
+                actuality. Hence, consider displaying your cutlery in a
+                fashionable way by using Akiiko’s cutlery case which comes in a
+                pack of six. This vegan fabric cutlery case is perfect when
+                you’re packing your lunch or simply making a statement with your
+                food. They also ensure that the cutlery remains free of dust.
               </p>
               <br />
               <div className='jone_img'>
@@ -115,13 +147,15 @@ ensure that the cutlery remains free of dust.
                 <img src='/images/blog_images_2/6.jpg' alt='jone_banner' />
               </div>
               <p>
-              Akiiko’s inspiration for kitchen decor has always been aimed at going minimal with one’s food
-habits and monochrome with one’s choice of style. The “Herra Placemats” ensures that you bid
-adieu to the temptation of overcrowding your dining table and switch to something more calm,
-composed and neutral.
+                Akiiko’s inspiration for kitchen decor has always been aimed at
+                going minimal with one’s food habits and monochrome with one’s
+                choice of style. The{' '}
+                <Link to='/product/herra-placemats'>“Herra Placemats”</Link>{' '}
+                ensures that you bid adieu to the temptation of overcrowding
+                your dining table and switch to something more calm, composed
+                and neutral.
               </p>
             </div>
-          
           </div>
           <div className='jone__key'>
             <span>Functional</span>
